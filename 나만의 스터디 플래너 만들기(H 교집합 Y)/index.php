4e76d2cfd85a51b7c651d29b32b37074927a342a
KIMdolphin <rlacofls0212@naver.com>
2020-02-24 01:12:31 +0900

    index.php의 분할과 main.css 독립

--- 나만의 스터디 플래너 만들기(H 교집합 Y)/index.php
+++ 나만의 스터디 플래너 만들기(H 교집합 Y)/index.php
@@ -1,105 +1,45 @@
 <html>
   <head>
-
     <meta charset = "utf-8">
      <link href="https://fonts.googleapis.com/css?family=Sunflower:300,700&display=swap" rel="stylesheet">
-<title>나만의 스터디 플래너 만들기</title>
-
-<style>
-body {
-  font-family: "Helvetica Neue", Helvetica, sans-serif;
- /* background: radial-gradient(white, #FFA328);*/
-  background-image: url('  https://live.staticflickr.com/65535/49000661446_0391667246_h.jpg');
-  /*
-  https://live.staticflickr.com/65535/49000657526_90696dfcbb_h.jpg(에펠탑 확대)
-  https://live.staticflickr.com/65535/49000661446_0391667246_h.jpg(커피 반투명)
-  */
-
-}
-
- .container {
- 	 text-align: center;
-   position: absolute;
-   top: 48%;
-   left: 43%;
-   transform: translate(-10%, -50%);
- }
-
-  #start_btn {
-  	position: absolute;
-    top: 80%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    border-radius: 13px;
-	  background-color: white ;
-    color: black;
-    border: 3px solid #7F574A;
-    padding: 40px 80px;
-    font-size: 40px;
-    font-family: 'BRBA_B', sans-serif;
-  }
-  #start_btn:hover {
-    background-color: #7F574A;
-    color: white;
-    border: 3px solid white;
-
-  }
-
-  p {
-    	text-align: center;
-    	font-family: 'GoyangIlsan', sans-serif;
-    	font-size: 25;
-    }
-
-  @font-face { font-family: 'BRBA_B'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_seven@1.2/BRBA_B.woff') format('woff'); font-weight: normal; font-style: normal; }
-
-
-  @font-face { font-family: 'SDMiSaeng'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/SDMiSaeng.woff') format('woff'); font-weight: normal; font-style: normal; }
-
-  @font-face { font-family: 'YiSunShinDotumM'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/YiSunShinDotumM.woff') format('woff'); font-weight: normal; font-style: normal; }
-
-@font-face { font-family: 'GoyangIlsan'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/GoyangIlsan.woff') format('woff'); font-weight: normal; font-style: normal; }
-</style>
+     <link href="css/main.css" rel="stylesheet"> <!-- main.css에 추가 효과 모아두었습니다.-->
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <title>나만의 스터디 플래너 만들기</title>
   </head>
-
   <body>
-    <header style="font-family: 'BRBA_B';font-size: 85; text-align: center; margin-top: 5%; margin-bottom: 2.5%; text-shadow: 2px 2px 2px gray;">나만의 플래너 만들기</header>
+  <div class="container">
+      <div class="row">
+          <div class="col"> <!-- 왼쪽 내용들입니다-->
+            <header style="font-family: 'BRBA_B';font-size: 45; text-align: center; margin-top: 5%; margin-bottom: 2.5%; text-shadow: 2px 2px 2px gray;">나만의 플래너 만들기</header>
+            <p>이 사이트에서는 자신만의 <strong>스터디 플래너</strong>를 제작할 수 있습니다!</p>
+            <p> 아래 <b>시작하기 버튼</b>을 누르고 나만의 스터디 플래너를 제작해보세요!!</p>
+            <p> <b>어디서나 계정만 있다면</b> 내가 만들었던 디자인을 인쇄, 수정할 수 있어요 </p>
+          </div>
+        <div class="col"> <!-- 오른쪽 내용들입니다-->
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </div>
+          </div>
+      </div>
+  </div>
+  <script>
 
-<p>이 사이트에서는 자신만의 <strong>스터디 플래너</strong>를 제작할 수 있습니다!</p>
+    window.onload = function() {
+      const startBtn = document.querySelector("#start_btn");
 
-
-
-<p> 아래 <b>시작하기 버튼</b>을 누르고 나만의 스터디 플래너를 제작해보세요!!</p>
-
-
-    <p> <b>pdf로 저장</b>시 인쇄가 가능하지만 다시 <b>수정</b>하지 못합니다!</p>
-
-<p> <b>중간 저장</b>시 다시 불러와 수정할 수 있습니다!</p>
-
-
-<input type = 'button' value = '시작하기' id = "start_btn" >
-
-
-
-
-<script>
-
-  window.onload = function() {
-    const startBtn = document.querySelector("#start_btn");
-
-    startBtn.addEventListener("click", function() {
-      var body = document.getElementsByTagName('body')[0];
-    	//$(body).animate({opacity: '0'}, {complete: d(body)}, 'slow');
-			body.outerHTML = '<h1 class = "container">Study Planner <br> Only For You</h1>'
-   		setTimeout('window.open("mainPlanner.php", "_self")', 2000);
-    });
-
-  }
-
-
-</script>
-
-
+      startBtn.addEventListener("click", function() {
+        var body = document.getElementsByTagName('body')[0];
+      	//$(body).animate({opacity: '0'}, {complete: d(body)}, 'slow');
+  			body.outerHTML = '<h1 class = "container">Study Planner <br> Only For You</h1>'
+     		setTimeout('window.open("mainPlanner.php", "_self")', 2000);
+      });
+    }
+  </script>
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
